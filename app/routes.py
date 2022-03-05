ed2cdc456a513ef332d1daf7a894f93f5c03c95e
Badr Choubai <badr.choubaidev@outlook.com>
2022-03-05 14:15:13 -0700

    Frontend wireframe

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,18 @@ from flask import render_template
 def index():
     return render_template("index.html")
 
+@app.route('/comment-demo')
+def comment_demo():
+    return render_template("comment-demo.html")
+
+@app.route('/user-platform-demo')
+def user_platform_demo():
+    return render_template("user-platform-demo.html")
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
 
 @app.route('/api/v1/resources/text')
 def api_index():
