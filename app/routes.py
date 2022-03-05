03520bbcce667d4b7698d6dcef90833a8e634e17
Heroku-Deploy <badr.choubaidev@outlook.com>
2022-03-05 15:19:43 -0700

    minor change to selection

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,23 @@
 from app import app
 from flask import render_template
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/comment-demo')
 def comment_demo():
     return render_template("comment-demo.html")
 
+
 @app.route('/user-platform-demo')
 def user_platform_demo():
-    return render_template("user-platform-demo.html")
+    data = {"options": [{"name": "Twitter", "icon_class": "bi-twitter"},
+                        {"name": "Reddit", "icon_class": "bi-reddit"}]}
+    return render_template("user-platform-demo.html", data=data)
+
 
 @app.route('/about')
 def about():
@@ -21,6 +27,7 @@ def about():
 @app.route('/api/v1/resources/text')
 def api_index():
     return ""
+
 
 @app.route('/api/v1/resources/social/:user/:platform')
 def api_user_platform():
