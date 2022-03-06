3159dd3303dbcda6934b134633f8ca68ecfe1d73
Heroku-Deploy <badr.choubaidev@outlook.com>
2022-03-05 18:56:25 -0700

    create api blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ import os
 
 from flask import Flask, render_template, request
 from werkzeug.datastructures import ImmutableMultiDict
+from app.api.api import api_bp
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    app.register_blueprint(api_bp)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -43,42 +45,5 @@ def create_app(test_config=None):
         return render_template("about.html")
 
 
-    text_parameters = [
-        "encoded_text"
-    ]
-
-    user_platform_parameters = [
-        "username",
-        "platform"
-    ]
-
-
-    def filter_requests(
-        requests: ImmutableMultiDict,
-        parameters: list[str]
-    ) -> dict:
-        # https://stackoverflow.com/questions/50396370/multiple-queries-in-one-elasticsearch-query
-        # Used for matching on multiple parameters at a time.
-        """Returns a dictionary containing all request parameters."""
-        print(requests, parameters)
-        parameter_list = [(key, requests.get(key)) for key in parameters]
-        return {key: value for key, value in parameter_list if value}
-
-
-    @app.route('/api/v1/resources/text', methods=["GET"])
-    def api_text():
-        args = filter_requests(request.args, text_parameters)
-        print(args)
-        return args
-
-
-    @app.route('/api/v1/resources/social', methods=["GET"])
-    def api_user_platform():
-        args = filter_requests(request.args, user_platform_parameters)
-        print(args)
-        return args
-
-
-        
     return app
 
