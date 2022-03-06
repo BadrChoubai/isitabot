42a2c1cf689a91da948df09b669d1f8601f489aa
Heroku-Deploy <badr.choubaidev@outlook.com>
2022-03-06 09:15:19 -0700

    fuck it, we'll do it live

--- app/json_scraper.py
+++ app/json_scraper.py
@@ -1,19 +1,19 @@
-import json
+# import json
 
-json_file = "midterm-2018.tsv"
+# json_file = "midterm-2018.tsv"
 
-# Python program to read
-# json file
+# # Python program to read
+# # json file
 
-# Iterating through the json
-# list
-with open(json_file, "r+") as file:
-    with open("bot_ids-twitter.txt", "w+") as bot_ids:
-        for line in file.readlines():
-            print(line.split("\t"))
-            if line.split("\t")[1] == "human\n":
-                break
+# # Iterating through the json
+# # list
+# with open(json_file, "r+") as file:
+#     with open("bot_ids-twitter.txt", "w+") as bot_ids:
+#         for line in file.readlines():
+#             print(line.split("\t"))
+#             if line.split("\t")[1] == "human\n":
+#                 break
 
-            bot_ids.write(line.split("\t")[0] + "\n")
+#             bot_ids.write(line.split("\t")[0] + "\n")
 
-# Closing file
+# # Closing file
