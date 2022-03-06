a8cf588f065d53cfc772a781fa8f995142f8cacc
Heroku-Deploy <badr.choubaidev@outlook.com>
2022-03-06 08:43:52 -0700

    clear

---
+++ app/api.py
@@ -0,0 +1,19 @@
+from flask import request, Blueprint
+from validation import filter_requests, parameters
+
+api_bp = Blueprint('api', __name__)
+
+
+@api_bp.route('/api/v1/resources/text', methods=["GET"])
+def api_text():
+    args = filter_requests(request.args, parameters["text_parameters"])
+    print(args)
+    return args
+
+
+@api_bp.route('/api/v1/resources/social', methods=["GET"])
+def api_user_platform():
+    args = filter_requests(
+        request.args, parameters["user_platform_parameters"])
+    print(args)
+    return args
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from venv import create
 from app import create_app
 
 if __name__ == "__main__":
     create_app()
-
