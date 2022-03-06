5ff6e9914f3a4e07cacb5299f2dd730883eb63fc
Heroku-Deploy <badr.choubaidev@outlook.com>
2022-03-06 11:17:36 -0700

    ops: api

--- api.py
+++ api.py
@@ -1,5 +1,8 @@
+from app import models
 from flask import request, Blueprint
 from validation import filter_requests, parameters
+import LM_probabilities
+import analyze
 
 api_bp = Blueprint('api', __name__)
 
@@ -7,8 +10,11 @@ api_bp = Blueprint('api', __name__)
 @api_bp.route('/api/v1/resources/text', methods=["GET"])
 def api_text():
     args = filter_requests(request.args, parameters["text_parameters"])
-    print(args)
-    return args
+    # print(form['encoded_text'])
+    result = analyze.interpret_result(analyze.estimate_sample(
+        LM_probabilities.run_all_tests(args['encoded_text'], models)))
+    # send text to probs
+    return result
 
 
 @api_bp.route('/api/v1/resources/social', methods=["GET"])
