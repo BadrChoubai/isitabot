6a1bd0edc18ca1617b25bbac681f72bdfac85906
Heroku-Deploy <badr.choubaidev@outlook.com>
2022-03-06 07:21:41 -0700

    twitter

--- app/src/api_interfaces/twitter.py
+++ app/src/api_interfaces/twitter.py
@@ -1,7 +1,8 @@
-import json
-import os
-from dotenv import load_dotenv
+from os import access
 import tweepy
+import time
+from dotenv import load_dotenv
+
 load_dotenv()
 
 consumer_key = API_KEY = os.environ.get('API_KEY')
@@ -9,20 +10,44 @@ consumer_secret = API_KEY_SECRET = os.environ.get('API_KEY_SECRET')
 access_token = ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 access_token_secret = ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 
-# authorization of consumer key and consumer secret
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-  
-# set access to user's access key and access secret 
-auth.set_access_token(access_token, access_token_secret)
-  
-# calling the api 
-api = tweepy.API(auth)
-  
-# using get_user with id
-_id = "103770785"
-user = api.get_user(_id)
-  
-# printing the name of the user
-print("The id " + _id +
-      " corresponds to the user with the name : " +
-      user.name)
+
+client = tweepy.Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret
+)
+
+
+def get_tweets(user_id: int):
+    tweets = client.get_users_tweets(
+        id=user_id, max_results=10, tweet_fields=["text"], user_auth=True)
+
+    return [tweet for tweet in tweets.data]
+
+
+# get to print each line in bot_ids-twitter.txt
+# once you can do that, change operation to get_tweets
+# in separate file, get working list export to file
+# once you have that, apply it to these lists
+start = time.time()
+
+user_tweets = []
+
+with open("user_ids-twitter.txt", "r") as bot_id_file:
+    for j in range(0, 10):
+        line = bot_id_file.readline()
+        if line:
+            print("---", line)
+            user_tweets.extend(get_tweets(int(line)))
+
+with open("user_tweets-twitter.txt", "w") as bot_tweets_file:
+    for i in len(user_tweets):
+        print(i)
+        for j in 10:
+            print(user_tweets[i][j])
+            # bot_tweets_file.write(user_tweets[i][j])
+
+end = time.time()
+
+time.sleep(900 - start - end)
