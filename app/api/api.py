cbac77e122a2ab33e92c031513d033380d5bd795
Heroku-Deploy <badr.choubaidev@outlook.com>
2022-03-05 20:23:49 -0700

    utils

--- app/api/api.py
+++ app/api/api.py
@@ -1,41 +1,18 @@
 from flask import request, Blueprint
-from werkzeug.datastructures import ImmutableMultiDict
+from src.utils.validation import filter_requests, parameters
 
 api_bp = Blueprint('api', __name__)
 
-text_parameters = [
-	"encoded_text"
-]
-
-user_platform_parameters = [
-	"username",
-	"platform"
-]
-
-
-
-def filter_requests(
-requests: ImmutableMultiDict,
-parameters: list[str]
-) -> dict:
-	# https://stackoverflow.com/questions/50396370/multiple-queries-in-one-elasticsearch-query
-	# Used for matching on multiple parameters at a time.
-	"""Returns a dictionary containing all request parameters."""
-	print(requests, parameters)
-	parameter_list = [(key, requests.get(key)) for key in parameters]
-	return {key: value for key, value in parameter_list if value}
-
-
 @api_bp.route('/api/v1/resources/text', methods=["GET"])
 def api_text():
-	args = filter_requests(request.args, text_parameters)
+	args = filter_requests(request.args, parameters["text_parameters"])
 	print(args)
 	return args
 
 
 @api_bp.route('/api/v1/resources/social', methods=["GET"])
 def api_user_platform():
-	args = filter_requests(request.args, user_platform_parameters)
+	args = filter_requests(request.args, parameters["user_platform_parameters"])
 	print(args)
 	return args
 
